c7bfbeb55b3d7d63f2eaaf9123ba759bebe3244b
KUSATAKE Daisuke <me@kusatake.dev>
2025-05-18 21:43:52 +0900

    liff init 処理をコンポーネントに分割

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import LiffApp from "./components/LiffApp"
 import App from "./App";
-
-import { liffInit } from './lib/liff-init'
-
-await liffInit()
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <LiffApp>
+      <App />
+    </LiffApp>
   </StrictMode>
 );
