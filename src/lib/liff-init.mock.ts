846266b35c88de5962552548ca4e9dd9e14d7c45
KUSATAKE Daisuke <me@kusatake.dev>
2025-05-18 20:39:49 +0900

    install @line/liff-mock

---
+++ src/lib/liff-init.mock.ts
@@ -0,0 +1,11 @@
+// LIFF Mock
+import { liff } from "@line/liff";
+import LiffMockPlugin from "@line/liff-mock";
+
+export const liffInit = async () => {
+  liff.use(new LiffMockPlugin());
+  await liff
+    .init({ liffId: import.meta.env.VITE_LIFF_ID, mock: true })
+  // liff.login() が呼ばれている必要があるため
+  if (!liff.isInClient()) liff.login()
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+import { liffInit } from './lib/liff-init'
+
+await liffInit()
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
