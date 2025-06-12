3f1cccdfec5cefe2bcd7f93ba93dc44aaa84f90f
KUSATAKE Daisuke <kusatake.daisuke@classmethod.jp>
2025-06-12 13:17:26 +0900

    liff.init() で withLoginOnExternalBrowser を利用するように変更しました

--- src/lib/liff-init/index.ts
+++ src/lib/liff-init/index.ts
@@ -2,10 +2,5 @@ import { liff } from "@line/liff";
 
 export const liffInit = async () => {
   return await liff
-    .init({ liffId: import.meta.env.VITE_LIFF_ID || "" })
-    .then(() => {
-      if (!liff.isLoggedIn() && !liff.isInClient()) {
-        liff.login();
-      }
-    });
+    .init({ liffId: import.meta.env.VITE_LIFF_ID, withLoginOnExternalBrowser: true });
 };
